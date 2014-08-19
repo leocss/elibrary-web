ef0ffd44017c45ce744766aa77a4a34da01305de
Laju Morrison <morrelinko@gmail.com>
2014-08-19 02:55:21 +0100

    Initial contribution

---
+++ app/bootstrap/application.php
@@ -0,0 +1,52 @@
+<?php
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+$app = new Silex\Application();
+$app['debug'] = true;
+$app->register(new \Silex\Provider\TwigServiceProvider(), [
+    'twig.path' => __DIR__ . '/../views'
+]);
+
+$app->get('/', function () use ($app) {
+    return $app['twig']->render('user/lock-screen.twig');
+});
+
+$app->get('/dashboard', function () use ($app) {
+    return $app['twig']->render('user/dashboard.twig');
+});
+
+$app->get('/books', function () use ($app) {
+    $books = json_decode(file_get_contents('http://127.0.0.1:4000/books'), true);
+
+    return $app['twig']->render('book/index.twig', [
+        'books' => $books
+    ]);
+});
+
+$app->get('/books/{id}', function ($id) use ($app) {
+        $book = json_decode(file_get_contents(sprintf('http://127.0.0.1:4000/books/%d', $id)), true);
+
+        return $app['twig']->render('book/view.twig', [
+            'book' => $book
+        ]);
+    }
+);
+
+$app->get('/printjobs', function () use ($app) {
+    return $app['twig']->render('printjob/index.twig');
+});
+
+$app->match('/printjobs/create', function (\Symfony\Component\HttpFoundation\Request $request) use ($app) {
+    if ($request->isMethod('post')) {
+        var_dump($request->request->all());
+    }
+
+    return $app['twig']->render('printjob/create.twig');
+});
+
+$app->match('/printjobs/{id}', function () use ($app) {
+    return $app['twig']->render('printjob/view.twig');
+});
+
+return $app;
