45b3215f1e15df68344d7093337b767161ea910b
Laju Morrison <morrelinko@gmail.com>
2014-10-27 19:21:45 +0100

    Setup e-test module

--- app/controllers/ElectronicTestCtrl.php
+++ app/controllers/ElectronicTestCtrl.php
@@ -18,6 +18,53 @@ class ElectronicTestCtrl extends BaseCtrl
      */
     public function index(Request $request)
     {
-        return $this->view->render('etest/index.twig');
+        $courses = $this->client->getEtestCourses();
+
+        return $this->view->render(
+            'etest/index.twig',
+            [
+                'courses' => $courses
+            ]
+        );
+    }
+
+    /**
+     * @param Request $request
+     * @param $course_id
+     * @return string
+     */
+    public function session(Request $request, $course_id)
+    {
+        $user = $this->client->getSessionUser();
+
+        // First create a session
+        $session = $this->client->createEtestSession(
+            [
+                'body' => [
+                    'course_id' => $course_id,
+                    'user_id' => $user['id'],
+                    'question_limit' => 20
+                ]
+            ]
+        );
+
+        // The session record returned from the previous request is
+        // used to perform another request to retrieve both the session details
+        // and all the questions associated with this session.
+        $session = $this->client->getEtestSession(
+            $session['id'],
+            [
+                'query' => [
+                    'include' => 'questions'
+                ]
+            ]
+        );
+
+        return $this->view->render(
+            'etest/session.twig',
+            [
+                'session' => $session
+            ]
+        );
     }
 }
